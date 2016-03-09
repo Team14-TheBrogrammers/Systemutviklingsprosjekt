6ca3d03b089c948313a90c2f942156ac9daccf52
ingunnsund <ingunnsu@online.no>
2016-03-09 13:26:41 +0100

    Main Client added
    MainClient class added.

---
+++ src/no/brogrammers/systemutviklingsprosjekt/MainClient.java
@@ -0,0 +1,10 @@
+package no.brogrammers.systemutviklingsprosjekt;
+
+/**
+ * Created by Ingunn on 09.03.2016.
+ */
+public class MainClient {
+    public static void main(String[] args) {
+        
+    }
+}
