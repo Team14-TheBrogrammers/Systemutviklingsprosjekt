776783154fe733eb47de01e8e2c5c187f405eec6
Knut Kirkhorn <knutkirk@hotmail.com>
2016-03-09 13:41:56 +0100

    DatabaseConnection class
    Added DatabaseConnection class in another branch,

---
+++ src/no/brogrammers/systemutviklingsprosjekt/DatabaseConnection.java
@@ -0,0 +1,10 @@
+package no.brogrammers.systemutviklingsprosjekt;
+
+/**
+ * Created by The Brogrammers on 09.03.2016.
+ */
+public class DatabaseConnection {
+    public DatabaseConnection() {
+
+    }
+}
