640532f79b8d307fc3831895f541e3319e9ae940
nicoleubk <nicole.ubk@outlook.com>
2016-04-15 11:58:00 +0200

    added recipe
    added recipe

--- src/main/java/no/brogrammers/systemutviklingsprosjekt/database/connectionclasses/RecipeConnection.java
+++ src/main/java/no/brogrammers/systemutviklingsprosjekt/database/connectionclasses/RecipeConnection.java
@@ -1,12 +1,112 @@
-package no.brogrammers.systemutviklingsprosjekt.database.connectionclasses;
+package no.brogrammers.systemutviklingsprosjekt.Database.ConnectionClasses;
 
-import no.brogrammers.systemutviklingsprosjekt.database.DatabaseConnection;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
 
 /**
- * Created by Knut on 07.04.2016.
+ * Created by Nicole on 07.04.2016.
+ *
  */
-public class RecipeConnection extends DatabaseConnection{
-    public RecipeConnection() {
-        super();
+public class RecipeConnection {
+
+    Connection connection;
+
+    public RecipeDAO(Connection connection) {
+        this.connection = connection;
     }
+
+    public boolean create(String recipeName, List<Ingredient> ingredients) {
+        if( !addRecipe(recipeName) ) {
+            return false;
+        }
+        if( !addIngredients(recipeName, ingredients) ) {
+            return false;
+        }
+        return addInstructions(recipeName);
+    }
+
+    private boolean addInstructions(String recipeName) {
+        return false;
+    }
+
+    private boolean addRecipe(String recipeName) {
+        try {
+            PreparedStatement pStatement = connection.prepareStatement(
+                    "INSERT INTO recipe(recipe_name) VALUES (?)"
+            );
+            pStatement.setString(1, recipeName);
+            pStatement.execute();
+            return true;
+        } catch(SQLException e) {
+            System.err.println(e);
+            return false;
+        }
+
+    }
+
+    private boolean addIngredients(String recipeName, List<Ingredient> ingredients) {
+        for (Ingredient ingredient : ingredients) {
+            try {
+                PreparedStatement pStatement = connection.prepareStatement(
+                        "INSERT INTO ingredients(ingredient_name) VALUES (?)"
+                );
+                pStatement.setString(1, ingredient.getIngredientName());
+                pStatement.execute();
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+
+            try {
+                PreparedStatement pStatement = connection.prepareStatement(
+                        "INSERT INTO recipe_ingredients(recipe_name, ingredient_name, quantity) VALUES (?,?,?)"
+                );
+                pStatement.setString(1, recipeName);
+                pStatement.setString(2, ingredient.getIngredientName());
+                pStatement.setString(3, ingredient.getQuantity());
+                pStatement.execute();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+
+        }
+        return true;
+    }
+
+    private boolean addInstructions(String recipeName, List<Instruction> instructions) {
+        for (Instruction instruction : instructions) {
+
+            try {
+                PreparedStatement pStatement = connection.prepareStatement(
+                        "INSERT INTO recipe_intructions(recipe_name, step_number, description) VALUES (?,?,?)"
+                );
+                pStatement.setString(1, recipeName);
+                pStatement.setInt(2, instruction.getStepNumber());
+                pStatement.setString(3, instruction.getDescription());
+                pStatement.execute();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+
+        }
+        return true;
+    }
+
+    public Recipe read() {
+        return null;
+    }
+
+    public boolean update() {
+        return false;
+    }
+
+    public boolean delete() {
+        return false;
+    }
+
+
 }
---
+++ src/main/java/no/brogrammers/systemutviklingsprosjekt/recipe/Recipe.java
@@ -0,0 +1,57 @@
+package no.brogrammers.systemutviklingsprosjekt.Recipe;
+
+import java.util.List;
+
+/**
+ * Created by Nicole on 07.04.2016.
+ */
+public class Recipe {
+
+
+    private String recipeName;
+    private RecipeType recipeType;
+    private List<String> ingredients;
+    private List<String> instructions;
+
+    public Recipe(String recipeName, RecipeType recipeType, List<String> ingredients, List<String> instructions) {
+        this.recipeName = recipeName;
+        this.recipeType = recipeType;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+    }
+
+    public String getRecipeName() { return recipeName; }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public RecipeType getRecipeType() {
+        return recipeType;
+    }
+
+    public void setRecipeType(RecipeType recipeType) {
+        this.recipeType = recipeType;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<String> getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(List<String> instructions) {
+        this.instructions = instructions;
+    }
+
+    public String toString() {
+        return recipeName + " (" + recipeType.name() + "):\n" + ingredients + "\n" + instructions;
+    }
+
+}
